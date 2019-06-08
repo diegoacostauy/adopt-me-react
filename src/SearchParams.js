2705b2faef75eba65d7a5bd38767980deaad9ea6
diegaco <diegoacosta211@gmail.com>
2019-06-08 14:05:29 -0300

    adding hooks in search, custom hook useDropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
   return (
     <div className="search-params">
       <form>
@@ -14,6 +20,8 @@ const SearchParams = () => {
             onChange={e => setLocation(e.target.value)}
           />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
